48d1fc6ac13e81ddaf6c65bb96e001f5cb46ee44
Lucas Garcia <lucasflorianogarcia@gmail.com>
2021-08-19 10:49:24 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/lucasgarcia/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.lucasgarcia.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasgarcia.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
