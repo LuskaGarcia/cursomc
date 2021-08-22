bd5f958920fd2d9dd7b9b34c087ec9011b85073c
Lucas Garcia <lucasflorianogarcia@gmail.com>
2021-08-22 20:26:22 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/lucasgarcia/cursomc/domain/Cliente.java
+++ src/main/java/com/lucasgarcia/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -91,6 +94,13 @@ public class Cliente implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -123,6 +133,8 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+
+	
 	
 	
 }
