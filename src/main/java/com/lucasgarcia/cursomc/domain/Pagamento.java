88c9a4b5f874f05929a55865e8109f18c9e271f1
Lucas Garcia <lucasflorianogarcia@gmail.com>
2021-08-22 21:46:24 -0300

    Atualização utilizando somente JsonIgnore

--- src/main/java/com/lucasgarcia/cursomc/domain/Pagamento.java
+++ src/main/java/com/lucasgarcia/cursomc/domain/Pagamento.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.lucasgarcia.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -24,7 +23,6 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
